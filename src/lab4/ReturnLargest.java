8fa0c8debebda0ec82cfa78aa63fc4d21c201d05
Peter Zhang <baiyan.zhang21@student.xjtlu.edu.cn>
2024-03-21 15:30:17 +0800

    ReturnLargest completed

--- src/lab4/ReturnLargest.java
+++ src/lab4/ReturnLargest.java
@@ -1,5 +1,18 @@
 package lab4;
 
-public class ReturnLargest {
+import java.util.ArrayList;
 
+public class ReturnLargest {
+    public static <E extends Comparable<E>> E max(ArrayList<E> list) {
+        if (list.size() == 0) {
+            return null;
+        }
+        E max = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i).compareTo(max) == 1) {
+                max = list.get(i);
+            }
+        }
+        return max;
+    }
 }
