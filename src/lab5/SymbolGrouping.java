78d069288fb8dd14c80be4e9df69ef0794d83a85
J1mMilton <151645462+J1mMilton@users.noreply.github.com>
2024-03-29 11:03:34 +0800

    SymbolGrouping completed

---
+++ src/lab5/SymbolGrouping.java
@@ -0,0 +1,51 @@
+package lab5;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class SymbolGrouping {
+
+    public static String group(String str) {
+        Stack<Character> parens = new Stack<>();
+        for(int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                parens.push('(');
+            }
+            try {
+                if (str.charAt(i) == ')') {
+                    parens.pop();
+                }
+            } catch (EmptyStackException e) {
+                return "Unpaired";
+            }
+
+            if (str.charAt(i) == '{') {
+                parens.push('{');
+            }
+            try {
+                if (str.charAt(i) == '}') {
+                    parens.pop();
+                }
+            } catch (EmptyStackException e) {
+                return "Unpaired";
+            }
+
+            if (str.charAt(i) == '[') {
+                parens.push('[');
+            }
+            try {
+                if (str.charAt(i) == ']') {
+                    parens.pop();
+                }
+            } catch (EmptyStackException e) {
+                return "Unpaired";
+            }
+        }
+        if (parens.empty()) {
+            return "Paired";
+        }
+        else return "Unpaired";
+
+    }
+
+}
---
+++ src/lab5/SymbolGroupingTest.java
@@ -0,0 +1,24 @@
+package lab5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SymbolGroupingTest {
+    @Test
+    public void testCase1() {
+        String str = "int a = (int)(\"0.1\");";
+        Assertions.assertEquals("Paired", SymbolGrouping.group(str));
+    }
+
+    @Test
+    public void testCase2() {
+        String str = "public void static main(String[] args){";
+        Assertions.assertEquals("Unpaired", SymbolGrouping.group(str));
+    }
+
+    @Test
+    public void emptyTest() {
+        String str = "";
+        Assertions.assertEquals("Paired", SymbolGrouping.group(str));
+    }
+}
